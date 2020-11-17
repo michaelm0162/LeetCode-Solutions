3eedd64321ed6ae88fe098dc56587b2453761768
michaelm0162 <74372449+michaelm0162@users.noreply.github.com>
2020-11-17 11:05:18 -0800

    Question 20. Valid Parentheses

---
+++ bracketValid.py
@@ -0,0 +1,47 @@
+def isValid(s):
+    '''s = string including brackets
+    return type: bool
+    verifies all brackets are valid by being closed correctly'''
+    bracket = []
+    openingBrackets = ['(', '[', '{']
+    closingBrackets = {')':'(', ']':'[', '}':'{'}
+    for i in range(len(s)):
+        if len(bracket) == 0 and s[i] in closingBrackets:
+            return False
+        if s[i] in closingBrackets:
+            if bracket[len(bracket)-1] != closingBrackets[s[i]]:
+                return False
+                quit()
+            else:
+                bracket.pop()
+        else:
+            if s[i] in openingBrackets:
+                bracket.append(s[i])
+    if len(bracket) == 0:
+        return True
+    else:
+        return False
+
+
+
+# TEST SCENARIOS
+s1 = '()[]{}'
+s2 = '([])'
+s3 = '(([[]]))'
+s4 = '(soooo[])'
+s5 = '([)]'
+s6 = '[([]])'
+s7 = '['
+s8 = '[{'
+s9 = ')]'
+
+print(isValid(s1))
+print(isValid(s2))
+print(isValid(s3))
+print(isValid(s4))
+print('Should be False:')
+print(isValid(s5))
+print(isValid(s6))
+print(isValid(s7))
+print(isValid(s8))
+print(isValid(s9))
